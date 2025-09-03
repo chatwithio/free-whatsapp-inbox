0827e9f4f242e7a4bf326b8b9333c5e025f2551e
chabieralaiza <calaiza64@gmail.com>
2025-09-03 15:03:21 +0200

    Restablecer el token de servicios en el entorno a la clave de ejemplo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: false,
     inboxApiBaseUrl: 'https://services.tochat.be/whatsapp-inbox',
-    //inboxApiBaseUrl: 'http://localhost:8000/whatsapp-inbox',
-    inboxServicesToken: 'e2558c3bba1193392beec378ab457c9f'
+    inboxServicesToken: 'YOUR_SERVICES_SECRET_KEY'
 };
